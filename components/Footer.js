a097cc73a274cba08ce6a0a33ba06062132578ea
Gabriel Marin <148821593+gahmarin@users.noreply.github.com>
2026-03-28 09:25:46 -0300

    Update Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#071A2F] text-center py-6 opacity-70">
+      © 2026 Jurisight - Todos os direitos reservados
+    </footer>
+  );
+}
